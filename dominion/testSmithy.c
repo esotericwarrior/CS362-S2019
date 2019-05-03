9a465d49a2a9e2c6e04a4bd445fa3acfb1ccd8ab
Tristan <santiatr@oregonstate.edu>
2019-05-03 01:26:56 -0400

    Updates
    Created testSmithy.c, updated test.Village.c

--- dominion/testSmithy.c
+++ dominion/testSmithy.c
@@ -4,12 +4,12 @@
 #include "rngs.h"
 #include "assert.h"
 
-/* Village (Action): Cost = 4, +1 Cards */
+/* Smithy (Action): Cost = 4, +3 Cards */
 
 int main(int argc, char** argv) {
 	int seed = 1000;
 	int numPlayer = 2;
-	int r, returnedResult, actions;
+	int r, returnedResult, actions, deckCount, deckCounter;
 	struct gameState G;
 	int choice1 = 0, choice2 = 0, choice3 = 0;
 
@@ -25,17 +25,32 @@ int main(int argc, char** argv) {
 	/* Set default number of actions. */
 	actions = G.numActions;
 
+	deckCount = G.handCount[1];	//Get current player's hand count
+	printf("Deck Count: %d\n", deckCount);
+
 	/* Since the cardEffect function returns 0 when it's successful, we assign
 	the returned number to a variable.*/
-	returnedResult = cardEffect(smithy, choice1, choice2, choice3, &G, NULL);
-	returnedResult = playSmithy()
-	// int handPos, int currentPlayer, struct gameState *state
+	//returnedResult = cardEffect(smithy, choice1, choice2, choice3, &G, NULL);
+	returnedResult = playSmithy(NULL, 1, &G);
+	//printf("Returned Result %d\n", returnedResult);	// Debugging.
+
+	deckCount = G.handCount[1];	//Get current player's hand count
+
+	if (DEBUG) {//Debug statements
+		printf("Current hand count: %d\n", deckCount);
+	}
+
+	deckCounter = G.deckCount[1];	//Create a holder for the deck count
+	printf("Deck Count: %d\n", deckCount);
+
+	if (deckCounter == 0)
+		return -1;
+
 	/* Check number of actions after card has been played. */
-	assert(G.numActions == actions + 2);
-	assert()
+	//assert(G.numActions == actions + 2);
 
 	/* Check returned value from cardEffect function. */
-	assert(returnedResult == 0);
+	//assert(returnedResult == 0);
 
 	printf("All tests passed!\n");
 	return 0;
