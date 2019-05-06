a94e8adb0de72a064ddb5036c774be60b9f1e282
Tristan <santiatr@oregonstate.edu>
2019-05-06 00:22:30 -0400

    Create unittest2.c

---
+++ dominion/unittest2.c
@@ -0,0 +1,15 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include "dominion.h"
+#include "rngs.h"
+#include "assert.h"
+#include "time.h"
+
+/* Cutpurse (Action - Attack): Cost = 4, +2 coins.
+Each other player discards a Copper (or reveals a hand with no Copper. */
+
+int main() {
+
+
+	return 0;
+}
