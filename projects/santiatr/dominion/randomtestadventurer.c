492142575fbf57b1f5af8b872ac36b5223d9de36
Tristan Santiago <37146990+esotericwarrior@users.noreply.github.com>
2019-05-13 13:48:41 -0400

    Create randomtestadventurer.c

---
+++ projects/santiatr/dominion/randomtestadventurer.c
@@ -0,0 +1,13 @@
+#include "dominion.h"
+#include "dominion_helpers.h"
+#include "rngs.h"
+#include <stdlib.h>
+#include <stdio.h>
+#include <time.h>
+
+int main(int argc, char** argv) {
+
+
+
+	return 0;
+}
