65e9214b103f7944584adebde2d4cdd1e06b0f08
Tristan Santiago <37146990+esotericwarrior@users.noreply.github.com>
2019-05-14 22:32:53 -0400

    Created randomcardtest2.c
    Working version

--- projects/santiatr/dominion/randomcardtest1.c
+++ projects/santiatr/dominion/randomcardtest1.c
@@ -1,54 +1,122 @@
-/***********************************************
-* Name: Tristan Santiago
-* Date: May 5, 2019
-* Description: cardtest1.c tests the Village
-* card.
-**********************************************/
-#include <stdlib.h>
-#include <stdio.h>
-#include "dominion.h"
-#include "rngs.h"
-#include "assert.h"
-#include <string.h> /* memset */
-
-/* Village (Action): Cost = 3, +1 Card, +2 Actions */
-
-int main(int argc, char** argv) {
-	int seed = 1000;
-	int numPlayer = 2;
-	int numTests = 100;
-	int i, r, returnedResult, actions;
-	struct gameState G;
-	int choice1 = 0, choice2 = 0, choice3 = 0;
-
-	int k[10] = { adventurer, council_room, feast, gardens, mine
-		, remodel, smithy, village, baron, great_hall };
-
-	for (i = 0; i < numTests; i++) {
-		/* Clear the game state. */
-		memset(&G, 23, sizeof(struct gameState));
-
-		/*Initialize a new game. */
-		r = initializeGame(numPlayer, k, seed, &G);
-
-		/* Set default number of actions. */
-		actions = G.numActions;
-
-		/* Since the cardEffect function returns 0 when it's successful, we assign
-		the returned number to a variable.*/
-		//returnedResult = cardEffect(village, choice1, choice2, choice3, &G, NULL);	// Village Card cardEffect test.
-		//printf("Returned Result %d\n", returnedResult);
-		returnedResult = playVillage(NULL, 1, &G);	// Refactored Village card test.
-													//printf("Returned Result %d\n", returnedResult);	// Debugging.
-
-													/* Check number of actions after card has been played. */
-		assert(G.numActions == actions + 2);
-
-		/* Check returned value from cardEffect function. */
-		assert(returnedResult == 0);
-	}
-
-	printf("All tests passed!\n");
-	return 0;
-
-}
+///***********************************************
+//* Name: Tristan Santiago
+//* Date: May 14, 2019
+//* Description: randomcardtest1.c tests the Village
+//* card.
+//**********************************************/
+//#include <stdlib.h>
+//#include <stdio.h>
+//#include "dominion.h"
+//#include "rngs.h"
+//#include "assert.h"
+//#include <string.h> /* memset */
+//
+///* Village (Action): Cost = 3, +1 Card, +2 Actions */
+//
+//int main(int argc, char** argv) {
+//	int seed = 1000;
+//	int numPlayer = 2;
+//	int numTests = 1;
+//	int i, j, l, r, returnedResult, actions;
+//	struct gameState G;
+//	int choice1 = 0, choice2 = 0, choice3 = 0;
+//
+//	int k[10] = { adventurer, council_room, feast, gardens, mine
+//		, remodel, smithy, village, baron, great_hall };
+//
+//	for (i = 0; i < numTests; i++) {
+//		/* Clear the game state. */
+//		memset(&G, 23, sizeof(struct gameState));
+//
+//		int numPlayers = rand() % (MAX_PLAYERS - 1) + 2;	// Random number of players between between 2 and 4.
+//		int currentPlayer = rand() % numPlayers + 1;
+//
+//		/*Initialize a new game. */
+//		r = initializeGame(numPlayers, k, seed, &G);
+//
+//		/* Set default number of actions. */
+//		actions = G.numActions;
+//
+//		/* Randomize deck size. */
+//		int deckSize = rand() % (MAX_DECK + 1);
+//		printf("Randomized deck size = %d\n", deckSize);
+//				
+//		/* Randomize hand size. */
+//		int handSize = rand() % (deckSize + 1);
+//		printf("Randomized hand size = %d\n", handSize);
+//		
+//		/* Calculate player one's hand size. */
+//		G.deckCount[currentPlayer] = deckSize - handSize;
+//		//printf("Deck count: %d\n", G.deckCount[currentPlayer]);
+//		
+//		/* Set player one's hand size. */
+//		G.handCount[currentPlayer] = handSize;
+//		
+//		/* Randomize discard pile count. */
+//		//G.discardCount[currentPlayer] = rand() % deckSize;
+//		G.discardCount[currentPlayer] = 0;
+//
+//		for (j = 0; j < numPlayers; j++) {
+//			for (l = 0; l < G.deckCount[j]; l++) {
+//				int randomCard = rand() % (50 + 1);
+//				int randomCard2 = rand() % (10);
+//				if (randomCard == 1) {
+//					G.deck[j][l] = copper;
+//				}
+//				else if (randomCard == 2) {
+//					G.deck[j][l] = silver;
+//				}
+//				else if (randomCard == 3) {
+//					G.deck[j][l] = gold;
+//				}
+//				else {
+//					G.deck[j][l] = k[randomCard2];
+//				}
+//			}
+//		}
+//
+//		int playedBefore = G.playedCardCount;
+//		printf("Played card count before: %d\n", playedBefore);
+//
+//		int deckBefore = G.deckCount[currentPlayer];
+//		printf("Deck before: %d\n", deckBefore);
+//
+//		//int discardBefore = G.discardCount[currentPlayer];
+//		//printf("Discard pile before: %d\n", discardBefore);
+//
+//		int handBefore = G.handCount[currentPlayer];
+//		printf("Hand before: %d\n", handBefore);
+//		
+//		/* Since the cardEffect function returns 0 when it's successful, we assign
+//		the returned number to a variable.*/
+//		int handPos = 0;
+//		returnedResult = playVillage(handPos, currentPlayer, &G);	// Refactored Village card test.
+//		//returnedResult = cardEffect(village, 1, 1, 1, &G, handPos);
+//		//printf("Returned Result %d\n", returnedResult);	// Debugging.
+//		
+//		/* Discard the card. */
+//		discardCard(handPos, currentPlayer, &G, 0);
+//
+//		int playedAfter = G.playedCardCount;
+//		printf("Played card count after: %d\n", playedAfter);
+//		
+//		int deckAfter = G.deckCount[currentPlayer];
+//		printf("Deck after: %d\n", deckAfter);
+//
+//		//int discardAfter = G.discardCount[currentPlayer];
+//		//printf("Discard pile after: %d\n", discardAfter);
+//
+//		int handAfter = G.handCount[currentPlayer];
+//		printf("Hand after: %d\n", handAfter);
+//
+//		/* Check number of actions after card has been played. */
+//		assert(G.numActions == actions + 2);
+//
+//		/* Check returned value from cardEffect function. */
+//		assert(returnedResult == 0);
+//	}
+//
+//	printf("All tests passed!\n");
+//	return 0;
+//
+//}
---
+++ projects/santiatr/dominion/randomcardtest2.c
@@ -0,0 +1,167 @@
+/***********************************************
+* Name: Tristan Santiago
+* Date: May 14, 2019
+* Description: randomcardtest2.c tests the
+* Council Room card.
+**********************************************/
+#include <string.h> /* memset */
+#include <stdlib.h>
+#include <stdio.h>
+#include "dominion.h"
+#include "rngs.h"
+#include "assert.h"
+#include "time.h"
+
+/* Council Room (Action): Cost = 5, +4 Cards, +1 Buy */
+/*------------------------------ Council Room Test ------------------------------*/
+int main(int argc, char** argv) {
+	int seed = 1000;
+	int numPlayer = 2;	// Number of players.
+	int numTests = 1000;	// Number of tests to run.
+	int currentPlayer = 0;	// Set current player to player 1.
+	int totalPassedTest = 0; // Variable used to track number of successful tests.
+	int i, r, returnedResult, actions, handPos, handSize, deckSize, deckCount, deckCounter;
+	int handCount, deck_before, hand_before, deck_after, hand_after, discard_pile_before, discard_pile_after;
+	int failed_hand_test = 0;
+	int failed_deck_test = 0;
+	int failed_discard_test = 0;
+	int failed_buy_test = 0;
+	struct gameState G;
+	int choice1 = 0, choice2 = 0, choice3 = 0;
+
+	int k[10] = { adventurer, council_room, feast, gardens, mine
+		, remodel, smithy, village, baron, great_hall };
+
+	/* Seed random number generator. */
+	srand(time(NULL));
+
+	for (i = 0; i < numTests; i++) {
+		/* Begin Test. */
+		printf("\nTest number %d\n\n", (i + 1));
+
+		/* Clear the game state. */
+		memset(&G, 23, sizeof(struct gameState));
+
+		/* Initialize a new game. */
+		r = initializeGame(numPlayer, k, seed, &G);
+
+		/* Randomize deck size. */
+		deckSize = rand() % (MAX_DECK + 1);
+		printf("Randomized deck size = %d\n", deckSize);
+
+		/* Randomize hand size. */
+		handSize = rand() % (deckSize + 1);
+		printf("Randomized hand size = %d\n", handSize);
+
+		/* Calculate player one's hand size. */
+		G.deckCount[0] = deckSize - handSize;
+		G.deckCount[1] = deckSize - handSize;
+
+		/* Set player one's hand size. */
+		G.handCount[0] = handSize;
+		G.handCount[1] = handSize;
+
+		handPos = 0;
+		/* Determine hand position of Council Room, so we discard properly. */
+
+		/* Here, we tally everything before playing Council Room. */
+
+		/* Get Player 1's hand count. */
+		hand_before = G.handCount[0];
+		printf("Hand count for player 1 prior to playing Council Room: %d\n", hand_before);
+		/* Player 1's deck count before playing Council Room. */
+		deck_before = G.deckCount[0];
+		printf("Deck count for Player 1 prior to playing Council Room: %d\n", deck_before);
+		/* Discard pile before playing Council Room. */
+		discard_pile_before = G.playedCardCount;
+		printf("Player 1's discard pile prior to playing Council Room: %d\n", discard_pile_before);
+		/* Get Player 1's number of buys before playing Council Room. */
+		int buys_before = G.numBuys;
+		printf("Player 1's number of buys before playing Council Room: %d\n", buys_before);	// Should be 1 by default.
+
+		/* Get Player 2's number of cards before playing Council Room. */
+		int p2_hand_before = G.handCount[1];
+		printf("Player 2's hand count prior to playing Council Room: %d\n", p2_hand_before);
+
+		/* Get Player 2's deck count before playing Council Room. */
+		int p2_deck_before = G.deckCount[1];
+
+
+		/* Since the cardEffect function returns 0 when it's successful, we assign the returned number to a variable.*/
+		//returnedResult = cardEffect(smithy, choice1, choice2, choice3, &G, NULL);
+		printf("Playing Council Room...\n");
+		returnedResult = cardEffect(council_room, 1, 1, 1, &G, handPos);	// Play Council Room card.
+
+		int buys_after = G.numBuys;
+		printf("Player 1's number of buys after playing Council Room: %d\n", buys_after);
+
+		/* Verify that Council Room was successfully played. */
+		//assert(returnedResult == 0);
+		//printf("Returned Result %d\n", returnedResult);	// Debugging.
+		
+		/* Now we check everything now that Council Room has been played. */
+		/* First, get player one's hand count after playing Council Room. */
+		hand_after = G.handCount[0];
+		printf("Hand count after playing Council Room: %d\n", hand_after);
+		printf("HAND count after playing Council Room: %d\n", G.handCount[0]);
+		printf("P2 hand count %d\n", G.handCount[1]);
+		/* Get player one's deck count after playing Council Room. */
+		deck_after = G.deckCount[0];
+		printf("Deck count after playing Council Room: %d\n", deck_after);
+		/* And finally, count discarded cards after playing Council Room. */
+		discard_pile_after = G.playedCardCount;
+		printf("Player 1's discard pile after playing Council Room: %d\n", G.playedCardCount);
+
+		int passedTest = 1;	// Boolean variable for passing or failing a test. True by default.
+
+		/*------------------------------ Test Results ------------------------------*/
+		/* Having played Council Room, we check player one's hand before and after. */
+		if (hand_after != (hand_before + 3)) {
+			printf("Incorrect number of cards drawn.\n");
+			/* Increment number of failed tests. */
+			failed_hand_test++;
+			/* Set test boolean to false. */
+			passedTest = 0;
+		}
+		/* No "else" statement used because it can be 4 cards drawn and showing correct. */
+
+		/* Check number of cards in the deck after Council Room is played. */
+		if (deck_after != (deck_before - 4)) {
+			printf("Incorrect number of cards in deck.\n");
+			/* Increment number of failed tests. */
+			failed_deck_test++;
+			/* Set test boolean to false. */
+			passedTest = 0;
+		}
+
+		if (discard_pile_after != (discard_pile_before + 1)) {
+			printf("Incorrect number of cards in discard pile.\n");
+			/* Increment number of failed tests. */
+			failed_discard_test++;
+			/* Set test boolean to false. */
+			passedTest = 0;
+		}
+
+		if (buys_after != buys_before + 1) {
+			printf("Incorrect number of buys assigned.\n");
+			failed_buy_test++;
+			passedTest = 0;
+		}
+
+		/* If all tests are passed, increment number of passed tests. */
+		if (passedTest == 1) {
+			printf("All tests passed!\n");
+			/* Increment total number of passed tests. */
+			totalPassedTest++;
+		}
+	}
+
+	/*------------------------------ Report Results ------------------------------*/
+	printf("\nTotal number of times all tests passed: %d\n", totalPassedTest);
+	printf("Total number of times cards drawn to hand failed: %d\n", failed_hand_test);
+	printf("Total number of times cards placed into deck failed: %d\n", failed_deck_test);
+	printf("Total number of times Council Room discarded incorrectly: %d\n", failed_discard_test);
+
+	return 0;
+
+}
